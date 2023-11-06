909e042b5eb8e89d0237e79384d4e0ac47b26b07
Patrick Borman <patrick.borman@imperial.ac.uk>
2023-11-06 17:14:13 +0000

    added no commits

--- api/app.py
+++ api/app.py
@@ -12,32 +12,40 @@ def hello_world():
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
-    response = requests.get(f"https://api.github.com/users/{input_name}/repos")
-    if response.status_code == 200:
-        repos = response.json()  # Data returned is a list of ‘repository’ entities
+    repos_response = requests.get(f"https://api.github.com/users/{input_name}/repos")
+    repos_data = repos_response.json()
+
+    repos_info = []
+    if repos_response.status_code == 200:
+        for repo in repos_data:
+            name = repo.get('name', 'N/A')
+            url = repo.get('html_url', '#')
+            updated_at = repo.get('updated_at', '')
+            if updated_at:
+                parsed_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
+                formatted_date = parsed_date.strftime("%B %d, %Y %H:%M:%S")
+            else:
+                formatted_date = 'N/A'
+
+            # Get the number of commits for this repo
+            commits_url = f"https://api.github.com/repos/{input_name}/{name}/commits"
+            commits_response = requests.get(commits_url)
+            commits_count = 'N/A'  # Default if commits can't be fetched
+            if commits_response.status_code == 200:
+                commits_data = commits_response.json()
+                commits_count = len(commits_data)
+
+            repo_info = {
+                'name': name,
+                'url': url,
+                'updated_at': formatted_date,
+                'commits_count': commits_count
+            }
+            repos_info.append(repo_info)
     else:
-        repos = [{"name": "Account not found"}]  # Ensure keys are used for consistency
+        repos_info = [{'name': "Account not found", 'url': '#', 'updated_at': 'N/A', 'commits_count': 'N/A'}]
 
-    repo_names = []
-    repo_urls = []
-    repo_updated_at = []
-
-    for r in repos:
-        repo_names.append(r.get('name', 'N/A'))  # Provide a default value in case 'name' is not present
-        repo_urls.append(r.get('html_url', '#'))  # 'html_url' is the URL to the repository on GitHub
-        updated_at = r.get('updated_at', '')
-        if updated_at:
-            # Parse the date string and reformat it
-            parsed_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
-            formatted_date = parsed_date.strftime("%B %d, %Y %H:%M:%S")  # E.g., "October 09, 2023 15:06:21"
-            repo_updated_at.append(formatted_date)
-        else:
-            repo_updated_at.append('N/A')
-
-    return render_template("hello.html", name=input_name, rnames=repo_names, rurls=repo_urls, rupdated=repo_updated_at)
+    return render_template("hello.html", name=input_name, repos_info=repos_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
