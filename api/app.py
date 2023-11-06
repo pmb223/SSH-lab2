e704bf977fbfeed25c025968c33028e5dd96afa5
Julius Gasson <jg2023@ic.ac.uk>
2023-11-06 15:50:50 +0000

    moved api call

--- api/app.py
+++ api/app.py
@@ -11,12 +11,9 @@ def hello_world():
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    user_ip = request.remote_addr  # Get the user's IP address
-    return render_template("hello.html", name=input_name, age=input_age, user_ip=user_ip)
+    response = requests.get("https://api.github.com/users/{username}/repos".format(input_name))
+    if response.status_code == 200:
+        repos = response.json() # data returned is a list of ‘repository’ entities
+    return render_template("hello.html", name=input_name)
 
-response = requests.get(“https://api.github.com/users/pmb223/repos”)
-if response.status_code == 200:
-    repos = response.json() # data returned is a list of ‘repository’ entities
-    for repo in repos:
-        print(repo[“full_name”])
+
