8ce10e5cedfaa98a84741b885364222712eb36cf
Patrick Borman <patrick.borman@imperial.ac.uk>
2023-11-06 15:38:08 +0000

    updated python

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -12,3 +14,9 @@ def submit():
     input_age = request.form.get("age")
     user_ip = request.remote_addr  # Get the user's IP address
     return render_template("hello.html", name=input_name, age=input_age, user_ip=user_ip)
+
+response = requests.get(“https://api.github.com/users/pmb223/repos”)
+if response.status_code == 200:
+    repos = response.json() # data returned is a list of ‘repository’ entities
+    for repo in repos:
+        print(repo[“full_name”])
