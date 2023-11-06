774228fb29b7568c3631f4675f5a5d1c57c1dfd0
Julius Gasson <jg2023@ic.ac.uk>
2023-11-06 15:55:37 +0000

    added repositories to submit page

--- api/app.py
+++ api/app.py
@@ -14,6 +14,8 @@ def submit():
     response = requests.get("https://api.github.com/users/{username}/repos".format(input_name))
     if response.status_code == 200:
         repos = response.json() # data returned is a list of ‘repository’ entities
-    return render_template("hello.html", name=input_name)
+    else:
+        repos = ["Account not found", None]
+    return render_template("hello.html", name=input_name, user_repos = repos)
 
 
